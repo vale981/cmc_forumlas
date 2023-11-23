4fae6b1440478a29bda172caa2b4167e293dcf50
Valentin Boettcher <hiro@protagon.space>
2023-11-23 16:06:09 -0500

    update formulas and add ipython notebook

--- cmc_formulas/formulas.py
+++ cmc_formulas/formulas.py
@@ -1,3 +1,9 @@
+"""
+Common formulas for capacitances, angular frequencies and inductances.
+
+**All frequencies are angular frequencies!**
+"""
+
 import numpy as np
 from . import ureg
 from . import constants
@@ -23,7 +29,7 @@ def c(v_0, Z_r):
     Capacitance per unit length as function of the speed of light
     ``v_0`` and typical impedance ``Z_r``..
     """
-    return (1 / (Z_r * v_0)).to(ureg.picofarad / ureg.meter)
+    return (1 / (Z_r * v_0)).to(ureg.femtofarad / ureg.meter)
 
 
 def l(v_0, Z_r):
@@ -39,7 +45,7 @@ def C_r(v_0, Z_r, d):
     Total capacitance of a CPW.  Like ``c`` but with an extra
     argument: the length ``d``.
     """
-    return (c(v_0, Z_r) * d).to(ureg.picofarad)
+    return (c(v_0, Z_r) * d).to(ureg.femtofarad)
 
 
 def L_r(v_0, Z_r, d):
@@ -66,18 +72,50 @@ def ω_q(E_C, E_J):
     return ((np.sqrt(8 * E_J * E_C) - E_C) / ureg.hbar).to(ureg.gigahertz)
 
 
+def ω_p(E_C, E_J):
+    """
+    The qubit (angular) frequency as a function as capacitive energy
+    ``E_C`` and junction energy ``E_J``.
+    """
+    return ((np.sqrt(8 * E_J * E_C)) / ureg.hbar).to(ureg.gigahertz)
+
+
 def E_C(C_Σ):
+    """
+    The capacitive energy of the transmon as a function of the total
+    transmon capacitance.
+    """
     return (ureg.elementary_charge**2 / (2 * C_Σ)).to(ureg.microjoule)
 
 
+def E_J_from_ω(ω, E_C):
+    """
+    The junction energy from qubit angular frequency and charging energy ``E_C``.
+    """
+    return (ω * ureg.hbar + E_C) ** 2 / (8 * E_C)
+
+
+def E_C_from_ω_and_ratio(ω, ratio):
+    """
+    The junction energy from qubit angular frequency and the ratio ``E_J/E_C``.
+    """
+    return (ω * ureg.hbar) / (np.sqrt(8 * ratio) - 1)
+
+
 def C_Σ(E_C):
-    return (ureg.elementary_charge**2 / (2 * E_C)).to(ureg.picofarad)
+    """The total transmon capacitance as a function of the capacitive energy."""
+    return (ureg.elementary_charge**2 / (2 * E_C)).to(ureg.femtofarad)
 
 
 # @ureg.wraps(
-#     ureg.picofarad, (ureg.second, ureg.picofarad, ureg.microjoule, ureg.hertz, ureg.ohm)
+#     ureg.femtofarad, (ureg.second, ureg.femtofarad, ureg.microjoule, ureg.hertz, ureg.ohm)
 # )
 def C_c(T_12, C_Σ, E_J, ω_B, Z_R=50 * ureg.ohm):
+    """
+    The coupling capacitance between the CPW and the transmon as a
+    function of the half-oscillation time, the transmon capacitance
+    ``C_Σ``, the transmon inductive energy ``E_J``.
+    """
     e_C = E_C(C_Σ)
     Δ = np.abs(ω_q(e_C, E_J) - ω_B)
     return (
@@ -86,14 +124,24 @@ def C_c(T_12, C_Σ, E_J, ω_B, Z_R=50 * ureg.ohm):
             / (4 * T_12 * ω_B**2 * Z_R)
             * np.sqrt(2 * e_C / E_J)
         )
-    ).to(ureg.picofarad)
+    ).to(ureg.femtofarad)
 
 
 def Δ(E_C, E_J, ω_B):
+    """
+    The qubit to bus CPW detuning as a function of the capacitive
+    energy of the transmon ``E_C`` the inductive energy of the
+    transmon ``E_J`` and the CPW frequency ``ω_B``.
+    """
     return np.abs(ω_q(E_C, E_J) - ω_B)
 
 
 def J(C_c, C_Σ, E_J, ω_B, Z_R=50 * ureg.ohm):
+    """
+    The coupling strength ``J`` of the target model (see notes) as a
+    function of bus CPW -- transmon coupling capacitance ``C_c``,
+    transmon capacitance ``C_Σ``, the inductive (junction) energy ``E_J``, the bus CPW frequency ``ω_B``
+    """
     e_C = E_C(C_Σ)
     return (
         ((4 * np.pi * ω_B**2 * C_c**2 * Z_R) / (Δ * C_Σ**2 * ureg.R_k))
@@ -114,5 +162,5 @@ def g(ω_0, C_Σ, C_c, E_J, Z_R=50 * ureg.ohm):
 
 def C_d_max(E_C, E_J, V_max):
     return ((2 * E_C / E_J) ** (1 / 4) * ureg.elementary_charge / (2 * V_max)).to(
-        ureg.picofarad
+        ureg.femtofarad
     )
--- cmc_formulas/typical_values.py
+++ cmc_formulas/typical_values.py
@@ -2,6 +2,7 @@ from . import ureg
 from . import constants
 from . import formulas
 import types
+import numpy as np
 
 
 CPW = types.SimpleNamespace()
@@ -9,16 +10,141 @@ CPW.Z_r = 50 * ureg.ohm
 CPW.v_0 = 1.3e8 * ureg.m / ureg.s
 CPW.d = 1 * ureg.cm
 CPW.l = formulas.c(CPW.v_0, CPW.Z_r)
-CPW.c =  formulas.l(CPW.v_0, CPW.Z_r)
+CPW.c = formulas.l(CPW.v_0, CPW.Z_r)
 CPW.C_r = formulas.C_r(CPW.v_0, CPW.Z_r, CPW.d)
 CPW.L_r = formulas.L_r(CPW.v_0, CPW.Z_r, CPW.d)
 CPW.ω_0 = formulas.ω_0(CPW.L_r, CPW.C_r)
 
 Transmon = types.SimpleNamespace()
-Transmon.E_C = 400 * ureg.megahertz * ureg.hbar
-Transmon.E_J = 25 * ureg.gigahertz * ureg.hbar
+Transmon.ratio = 50
+Transmon.E_C = 100 * ureg.megahertz * ureg.hbar * 2 * ureg.pi
+Transmon.E_J = Transmon.ratio * Transmon.E_C
 Transmon.C_Σ = formulas.C_Σ(Transmon.E_C)
 Transmon.ω_q = formulas.ω_q(Transmon.E_C, Transmon.E_J)
 Transmon.α = (Transmon.E_C / ureg.hbar).to(ureg.gigahertz)
 
 V_Dmax = 100 * ureg.microvolt
+
+
+class CoplanarWaveguide:
+    def __init__(self, impedance=CPW.Z_r, speed_of_light=CPW.v_0, length=CPW.d):
+        self._impedance = impedance
+        self._speed_of_light = speed_of_light
+        self._length = length
+
+    @property
+    def L(self):
+        return formulas.L_r(self._speed_of_light, self._impedance, self._length)
+
+    @property
+    def C(self):
+        return formulas.C_r(self._speed_of_light, self._impedance, self._length)
+
+    @property
+    def ω(self):
+        return formulas.ω_0(self.L, self.C)
+
+    @property
+    def f(self):
+        return formulas.ω_0(self.L, self.C) / (2 * np.pi)
+
+    @property
+    def Z(self):
+        return self._impedance
+
+
+class TransmonQubit:
+    def __init__(self, E_C=Transmon.E_C, E_J=Transmon.E_J):
+        self._charge_energy = E_C.to(ureg.gigahertz * ureg.hbar)
+        self._junction_energy = E_J.to(ureg.gigahertz * ureg.hbar)
+
+    def __repr__(self):
+        return f"""
+{self.__class__.__name__}({self.E_C}, {self.E_J})
+        """
+
+    def Δ(self, cpw: CoplanarWaveguide):
+        return self.ω - cpw.ω
+
+    def g(self, cpw: CoplanarWaveguide, C_c):
+        return formulas.g(cpw.ω, self.C_Σ, C_c, self.E_J, cpw.Z)
+
+    def overview(self):
+        return f"""ω:      {self.ω}
+ω/2π:   {self.f}
+E_J:    {self.E_J}
+E_C:    {self.E_C}
+f_J:    {self.f_J}
+f_C:    {self.f_C}
+ratio:  {self.ratio}"""
+
+    @property
+    def E_C(self):
+        return self._charge_energy
+
+    @property
+    def f_C(self):
+        return self._charge_energy.to(ureg.gigahertz * ureg.planck_constant)
+
+    @E_C.setter
+    def E_C(self, E_C):
+        self._charge_energy = E_C.to(ureg.gigahertz * ureg.hbar)
+
+    @property
+    def E_J(self):
+        return self._junction_energy
+
+    @property
+    def f_J(self):
+        return self._junction_energy.to(ureg.gigahertz * ureg.planck_constant)
+
+    @E_J.setter
+    def E_J(self, E_J):
+        self._junction_energy = E_J.to(ureg.gigahertz * ureg.hbar)
+
+    @property
+    def C_Σ(self):
+        return formulas.C_Σ(self.E_C).to(ureg.femtofarad)
+
+    @property
+    def ratio(self):
+        return self.E_J / self.E_C
+
+    @property
+    def ω(self):
+        return formulas.ω_q(self._charge_energy, self._junction_energy)
+
+    @property
+    def f(self):
+        return self.ω / (2 * np.pi)
+
+    @classmethod
+    def from_capacitances(cls, C_Σ=formulas.C_Σ(Transmon.E_C), E_J=Transmon.E_J):
+        return cls(formulas.E_C(C_Σ), E_J)
+
+    @classmethod
+    def from_angular_frequency_and_charging_energy(
+        cls, ω_q=Transmon.ω_q, E_C=Transmon.E_C
+    ):
+        return cls(E_C, formulas.E_J_from_ω(ω_q, E_C))
+
+    @classmethod
+    def from_angular_frequency_and_transmon_ratio(
+        cls, ω_q=Transmon.ω_q, ratio=Transmon.ratio
+    ):
+        charge_energy = formulas.E_C_from_ω_and_ratio(ω_q, ratio)
+        return cls(charge_energy, charge_energy * ratio)
+
+
+def dimless_mag(quantity):
+    return quantity.to(ureg.dimensionless).magnitude
+
+
+def dispersive_metrics(transmon: TransmonQubit, cpw: CoplanarWaveguide, C_c):
+    return {
+        "Anharmonicity, α/Δ": dimless_mag(transmon.E_C / (transmon.Δ(cpw) * ureg.hbar)),
+        "Coupling Strength, g/Δ": dimless_mag(transmon.g(cpw, C_c) / transmon.Δ(cpw)),
+        "RWA counter-rotating": dimless_mag(transmon.Δ(cpw) / (transmon.ω + cpw.ω)),
+        "Coupling/Transmon Cap": dimless_mag(C_c / transmon.C_Σ),
+        "Coupling/CPW Cap": dimless_mag(C_c / cpw.C),
+    }
